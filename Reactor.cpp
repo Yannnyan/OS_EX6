ed3f7f2c1447f92544b77ea1b903636a078434ed
Yan <regoner2@gmail.com>
2022-05-30 10:01:29 +0300

    reactor hpp skeleton

---
+++ Reactor.cpp
@@ -0,0 +1,24 @@
+#include "Reactor.hpp"
+
+
+
+void * ex6::Reactor::newReactor()
+{
+    
+}
+void ex6::Reactor::InstallHandler(void (*func)(), int fd)
+{
+
+}
+void ex6::Reactor::RemoveHandler(void * reactor)
+{
+
+}
+
+
+
+
+
+
+
+
--- Reactor.hpp
+++ Reactor.hpp
@@ -5,7 +5,13 @@ namespace ex6
 {
     class Reactor
     {
-        
+        private:
+            
+        public:
+            void * newReactor();
+            void InstallHandler(void (*func)(), int fd);
+            void RemoveHandler(void * reactor);
+
     };
 }
 
